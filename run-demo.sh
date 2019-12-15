72c34d1d456568dee08e64512ea0f7e259c0554b
Albert Asawaroengchai <teerakarna@users.noreply.github.com>
2019-12-15 20:23:48 +0000

    explicit mention of /etc/hosts

--- run-demo.sh
+++ run-demo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Check dependencies are installed; doesn't care about versions
-dep_list=( git docker minikube kubectl )
+dep=( git docker minikube kubectl )
 
-for i in "${dep_list[@]}"; do
+for i in "${dep[@]}"; do
 	if ! command -v "$i" >/dev/null 2>&1; then
     	echo -e "Error: Can not find \"$i\" in environment variable $PATH"
     	exit 1
@@ -46,7 +46,7 @@ kubectl expose deployment sinatra-demo --type=NodePort --port=4567 && \
 kubectl apply -f manifests/ingress.yaml && \
 
 # Output message to user for final manual configuration step
-echo -e "\nAdd this to the end of your host file:\n\n\t$(kubectl get ingress sinatra-demo \
+echo -e "\nAdd this to the end of your /etc/hosts file:\n\n\t$(kubectl get ingress sinatra-demo \
 	-ojsonpath='{.status.loadBalancer.ingress[].ip}') $(kubectl get ingress sinatra-demo \
     -ojsonpath='{.spec.rules[].host}')\n\nAccess site via this URL:\n\n\thttp://$(kubectl get ingress sinatra-demo \
     -ojsonpath='{.spec.rules[].host}')\n"
